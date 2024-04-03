1f83eba3776e84ce4e28aaf9c405b338d06d5d3d
Rajiv Soren <103196757+iam-Red@users.noreply.github.com>
2024-04-03 21:37:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ model = joblib.load('logistic_regression_model.pkl')
 def form():
     return render_template('predict.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 # Handle form submission and provide prediction
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -152,4 +156,4 @@ def predict():
     return render_template('result.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run()
+    app.run
