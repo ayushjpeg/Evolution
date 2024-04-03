c5b00576bd46c4087e93386ce7baac6d2810d559
swarajbharadwaj <114842077+swarajbharadwaj@users.noreply.github.com>
2024-04-03 09:20:56 +0000

    3rd commit

--- app.py
+++ app.py
@@ -48,57 +48,59 @@ def predict():
         # Habitat
         habitat = request.form['Habitat']
         features.extend([1 if habitat == 'forest' else 0,
-                        1 if habitat == 'mixed' else 0,
-                        1 if habitat == 'forest-gallery' else 0])
+                        1 if habitat == 'forest-gallery' else 0,
+                        1 if habitat=='forest-savanna' else 0,
+                        1 if habitat=='jungle' else 0,
+                         1 if habitat == 'mixed' else 0])
 
         
         # Incisor Size
         incisor_size = request.form['Incisor_Size']
-        features.extend([1 if incisor_size == size else 0 for size in ['small', 'big', 'megadony', 'very small', 'medium large']])
+        features.extend([1 if incisor_size == size else 0 for size in ['medium large','megadony','small', 'very small' ]])
 
         # Jaw Shape
         jaw_shape = request.form['Jaw_Shape']
-        features.extend([1 if jaw_shape == shape else 0 for shape in ['U shape', 'conical', 'V shape', 'modern']])
+        features.extend([1 if jaw_shape == shape else 0 for shape in ['V shape','conical', 'modern']])
 
         # Torus Supraorbital
         torus_supraorbital = request.form['Torus_Supraorbital']
-        features.extend([1 if torus_supraorbital == torus else 0 for torus in ['very protruding', 'little protruding', 'less protruding', 'ultra protruding', 'flat']])
+        features.extend([1 if torus_supraorbital == torus else 0 for torus in ['less protruding','little protruding','ultra protruding', 'very protruding']])
 
         # Prognathism
         prognathism = request.form['Prognathism']
-        features.extend([1 if prognathism == p else 0 for p in ['absent', 'medium', 'medium-high', 'high', 'very high', 'reduced']])
+        features.extend([1 if prognathism == p else 0 for p in [ 'high','medium', 'medium-high', 'reduced', 'very high' ]])
 
         # Foramen MAignum Position
         foramen_maignum = request.form['Foramen_MAignum_Position']
-        features.extend([1 if foramen_maignum == pos else 0 for pos in ['posterior', 'semi-anterior', 'anterior', 'modern']])
+        features.extend([1 if foramen_maignum == pos else 0 for pos in ['modern','posterior', 'semi-anterior' ]])
 
         # Canine Size
         canine_size = request.form['Canine_Size']
-        features.extend([1 if canine_size == size else 0 for size in ['small', 'big']])
+        features.extend([1 if canine_size == size else 0 for size in ['small']])
 
         # Canines Shape
         canines_shape = request.form['Canines_Shape']
-        features.extend([1 if canines_shape == shape else 0 for shape in ['conicalls', 'incisiform']])
+        features.extend([1 if canines_shape == shape else 0 for shape in [ 'incisiform']])
 
         # Tooth Enamel
         tooth_enamel = request.form['Tooth_Enamel']
-        features.extend([1 if tooth_enamel == enamel else 0 for enamel in ['thin', 'thick', 'very thin', 'very thick', 'medium thin', 'medium thick', 'thick medium']])
+        features.extend([1 if tooth_enamel == enamel else 0 for enamel in [ 'medium thin','thick','thick medium','thin','very thick',  'very thin' ]])
 
         # Tecno
         tecno = request.form['Tecno']
-        features.extend([1 if tecno == t else 0 for t in ['yes', 'no', 'likely']])
+        features.extend([1 if tecno == t else 0 for t in ['no', 'yes']])
 
         # Tecno Type
         tecno_type = request.form['Tecno_type']
-        features.extend([1 if tecno_type == t else 0 for t in ['no', 'mode 1', 'mode 2', 'mode 3', 'mode 4', 'primitive']])
+        features.extend([1 if tecno_type == t else 0 for t in [ 'mode 2', 'mode 3', 'mode 4', 'no', 'primitive']])
 
         # Biped
         biped = request.form['biped']
-        features.extend([1 if biped == b else 0 for b in ['yes', 'low probability', 'high probability', 'modern']])
+        features.extend([1 if biped == b else 0 for b in [ 'low probability', 'modern','yes']])
 
         # Arms
         arms = request.form['Arms']
-        features.extend([1 if arms == a else 0 for a in ['climbing', 'walk', 'manipulate']])
+        features.extend([1 if arms == a else 0 for a in [ 'manipulate','manipulate with precision', 'walk']])
 
         # Foots
         foots = request.form['Foots']
@@ -106,31 +108,31 @@ def predict():
 
         # Diet
         diet = request.form['Diet']
-        features.extend([1 if diet == d else 0 for d in ['dry fruits', 'soft fruits', 'hard fruits', 'omnivore', 'carnivore']])
+        features.extend([1 if diet == d else 0 for d in ['dry fruits',  'hard fruits', 'omnivore','soft fruits' ]])
 
         # Sexual Dimorphism
         sexual_dimorphism = request.form['Sexual_Dimorphism']
-        features.extend([1 if sexual_dimorphism == s else 0 for s in ['High', 'Medium', 'Medium_High', 'Reduced']])
+        features.extend([1 if sexual_dimorphism == s else 0 for s in [ 'medium-High', 'reduced']])
 
         # Hip
         hip = request.form['Hip']
-        features.extend([1 if hip == h else 0 for h in ['slim', 'wide', 'modern', 'very modern']])
+        features.extend([1 if hip == h else 0 for h in ['slim', 'very modern', 'wide']])
 
         # Verical Front
         verical_front = request.form['Verical_Front']
-        features.extend([1 if verical_front == v else 0 for v in ['yes', 'no', 'modern']])
+        features.extend([1 if verical_front == v else 0 for v in ['no','yes']])
 
         # Anatomy
         anatomy = request.form['Anatomy']
-        features.extend([1 if anatomy == a else 0 for a in ['old', 'mixed', 'modern', 'very modern']])
+        features.extend([1 if anatomy == a else 0 for a in [ 'modern','old', 'very modern']])
 
         # Migrated
         migrated = request.form['Migrated']
-        features.extend([1 if migrated == m else 0 for m in ['yes', 'no']])
+        features.extend([1 if migrated == m else 0 for m in ['yes']])
 
         # Skeleton
         skeleton = request.form['Skeleton']
-        features.extend([1 if skeleton == s else 0 for s in ['light', 'refined', 'robust']])
+        features.extend([1 if skeleton == s else 0 for s in ['refined', 'robust']])
     except:
         pass
     # Make prediction
